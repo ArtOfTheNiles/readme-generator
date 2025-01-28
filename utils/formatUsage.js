dc6042e70b5631ef174fcf65756fd01dd4a104fd
Niles Bontrager <artoftheniles@yahoo.com>
2025-01-28 17:19:12 -0500

    Video Embed removed, will wait for CLI functionality before reimpliment

--- utils/formatUsage.js
+++ utils/formatUsage.js
@@ -2,7 +2,7 @@
 const videoLink = `<iframe title="vimeo-player" src="https://player.vimeo.com/video/1035076639?h=01f084c303" width="640" height="360" frameborder="0" allowfullscreen></iframe>`
 // Expects a string
 export const formatUsage = ( usage ) => {
-    return `## Usage\n\n${usage}\n${videoLink}\n`;
+    return `## Usage\n\n${usage}\n`;
 };
 
 export default formatUsage;
